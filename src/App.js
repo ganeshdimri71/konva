e35aa9416b202e629cc4ee42c6f8c80413171c7f
ganesh-gamd <dimri@gamorgan.in>
2022-03-28 11:27:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import { useSelector } from 'react-redux';
 
 function App () {
   const [ circleArray, setCircleArray ] = useState( [] )
+  const stageRef = React.useRef();
+  const layerRef = React.useRef();
+  const groupRef = React.useRef();
+  const rectRef = React.useRef();
+  const circleRef = React.useRef();
   const handleStageClick = ( e ) => {
-    let { x, y } = e.target.getStage().getPointerPosition();
+    let mousePos = e.target.getStage().getPointerPosition();
+    const a = groupRef.current.getAbsoluteTransform().copy()
+    a.invert();
+    let { x, y } = a.point( mousePos )
     setCircleArray( [ ...circleArray, [ x, y ] ] )
-    console.log( 'circleArray', circleArray )
-    console.log( x, y )
   }
+
+
   return (
     <>
       <Stage width={ window.innerWidth }
         height={ window.innerHeight }
         style={ { backgroundColor: '#e3e1e1' } }
         onClick={ handleStageClick }
+        ref={ stageRef }
       >
-        <Layer>
+        <Layer
+          ref={ layerRef }
+        >
           <Group
             draggable
+            ref={ groupRef }
+            x={ 0 }
+            y={ 0 }
           >
             <Rect
               fill={ 'yellow' }
@@ -28,26 +42,35 @@ function App () {
               width={ 300 }
               height={ 300 }
               shadowBlur={ 10 }
+              ref={ rectRef }
             />
             { circleArray.map( ( item, index ) => (
               <Circle
                 key={ index }
-                x={ item[ 0 ] }
-                y={ item[ 1 ] }
+                x={ item[ 0 ]
+
+                }
+                y={ item[ 1 ]
+
+                }
                 fill={ "red" }
                 radius={ 20 }
+                ref={ circleRef }
+                onClick={ ( e ) => {
+                  e.cancelBubble = true;
+                } }
               />
             ) ) }
 
           </Group>
         </Layer>
       </Stage>
+      <button
+        // onClick={ () => getTransform() }
+        style={ { position: "absolute", top: "0", left: '50%' } }
+      >Transform</button>
     </>
-
   )
-
 }
-
-
 export default ( App );
 
